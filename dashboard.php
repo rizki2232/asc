f8e34fef6c1dd2160b90aa173af677dcd8894b04
E41221941 Rizki Makhfud Nur'Atsani <e41221941@student.polije.ac.id>
2025-02-18 09:34:11 +0700

    rizki

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pt acs</title>
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="style.css" />
 </head>
 
