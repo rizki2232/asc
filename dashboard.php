c19778b294a6a604ceb80f6547b2b25d7950bec6
E41221941 Rizki Makhfud Nur'Atsani <e41221941@student.polije.ac.id>
2025-02-18 08:16:23 +0700

    first

---
+++ dashboard.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pt acs</title>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div class="container">
+        <nav>
+            <img src="image/logo.png" alt="">
+            <ul>
+                <li><a href="">BERANDA</a></li>
+                <li><a href="">PROFILE</a></li>
+                <li><a href="">PARTNERSHIP</a></li>
+                <li><a href="">SERVICE</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+
+</html>
